274d6b4e3f6694042215e1263e7f83d10e9527c7
ANNE <afunnilo@gmail.com>
2021-08-15 19:35:03 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+     <header>
+         <h1>{{ title}}</h1>
+         <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="steelblue" />
+     </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+ export default {
+    name:'Header',
+    props:{
+        title: String
+    },
+    components: {
+        Button
+    }
+ }
+</script>
+
+<style scoped>
+   header{
+       display:flex;
+       justify-content: space-between;
+       align-items: center;
+       margin-bottom: 20px;
+
+   } /* scoped means anything in this style tag will only be for this particular component */
+  
+ 
+ 
+</style>
